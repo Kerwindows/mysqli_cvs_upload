9e3d908db96f70111ac36fa771e8f4be60250e61
Kerwin Thompson <kerwindows@hotmail.com>
2021-11-24 19:55:30 -0500

    html fixes

--- index.php
+++ index.php
@@ -11,12 +11,12 @@ $import_csv->import($_FILES['file']['tmp_name']);
 <html>
   <body>
     <form method="post" enctype="multipart/form-data">
-      <div class="info-box-content "> <span class="info-box-text"><p class="">Upload User List</p>
+      <div class=""> <span class=""><p class="">Upload User List</p>
                           <input  type="file" name="file" required="required" />
-                          </span> </div>
-      <div class="btn-group pull-right">
-        <button type="submit" class="btn btn-sm btn-success " type="submit" name="import_csv" value="Import">Import</button>
-        <!--<input class="btn btn-success pull-right" type="submit" name="sub" value="Import" />-->
+                          </span>
+      </div>
+      <div class="">
+        <button type="submit" class="" type="submit" name="import_csv" value="Import">Import</button>
     </form>
   </body>
 </html>
